9bb3d372e20ae540142cf016da6e93b1b6b8ef24
MustafaSamiShirzad <mustafasami209@gmail.com>
2023-06-20 02:01:02 +0500

    Modified Js files

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const menu_button = document.querySelector(".navBtnMenu");
 const mobile_menu = document.querySelector(".nav__mobile_menu");
 const nav_menu = document.querySelector(".header_bar");
+const nav__mobile_menu = document.querySelector(".nav__mobile_menu");
 
 menu_button.addEventListener("click", ()=>{
    mobile_menu.style.display = "flex";
    nav_menu.style.display = 'none';
 });
+nav__mobile_menu.addEventListener("click",()=>{
+   mobile_menu.style.display = "none";
+   nav_menu.style.display = 'block';
+})
