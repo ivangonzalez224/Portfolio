011b395db5cfcacc4d667bccb2d812a1283d4938
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-21 22:03:30 -0500

    Add Headline structure

---
+++ src/components/head/Headline.jsx
@@ -0,0 +1,24 @@
+const Headline = () => (
+  <div>
+    <div id="head-name">
+      <span>
+        Hello, I am
+      </span>
+      <h2>Iván</h2>
+      <span>
+        a software developer crafting modern and aesthetically pleasing web sites.
+      </span>
+    </div>
+    <div id="head-photo">
+      <img className="nav_left" />
+      <div>
+        <ul>
+            <li>Team Collaboration</li>
+            <li>Responsive Design</li>
+        </ul>
+      </div>
+    </div> 
+  </div>
+);
+
+export default Headline;
--- src/components/pages/main/Home.jsx
+++ src/components/pages/main/Home.jsx
@@ -1,6 +1,9 @@
+import Headline from '../../head/Headline';
+
 const Home = () => (
     <div className="main_home">
       <h1>home</h1>
+      <Headline />
     </div>
   );
   
