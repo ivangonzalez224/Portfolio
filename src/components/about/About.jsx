6a30a4b05e7e931ea5a15ad820bc4a09d0073c89
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-24 10:09:30 -0500

    Add the about section structure

---
+++ src/components/about/About.jsx
@@ -0,0 +1,34 @@
+import '../../assets/styles/about.css';
+
+
+const About = () => (
+  <div className="about-main">
+    <div id="about-presentation">
+      <span>
+        Hello
+      </span>
+      <p>
+        My name is Iván Gonzalez, and I am a pasionate Full Stack Web Developer fousing on solving
+        problems for different industries using the power of technology 
+      </p>
+      <p>
+        I would love to hear from you, whether is a project, job opportunity or just a chat. 
+        Feel free to contact me.
+      </p>
+    </div>
+    <div id="about-skills">
+      <span>Skills</span>  
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Jest</li>
+        <li>React & Redux</li>
+        <li>Ruby on Rails</li>
+        <li>PostgreSQL</li>
+      </ul> 
+    </div> 
+  </div>
+);
+
+export default About;
--- src/components/pages/main/Home.jsx
+++ src/components/pages/main/Home.jsx
@@ -1,8 +1,10 @@
 import Headline from '../../head/Headline';
+import About from '../../about/About';
 
 const Home = () => (
     <div className="main_home">
       <Headline />
+      <About />
     </div>
   );
   
