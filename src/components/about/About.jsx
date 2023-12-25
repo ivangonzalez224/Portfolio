a312a7ee032aa4ee9abc4d682d01104224c1e628
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-25 14:04:59 -0500

    Add desktop version styles to the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,31 +3,44 @@ import '../../assets/styles/about.css';
 
 const About = () => (
   <div className="about-main">
-    <div id="about-presentation">
-      <span>
-        Hello
-      </span>
-      <p>
-        My name is Iván Gonzalez, and I am a pasionate Full Stack Web Developer fousing on solving
-        problems for different industries using the power of technology 
-      </p>
-      <p>
-        I would love to hear from you, whether is a project, job opportunity or just a chat. 
-        Feel free to contact me.
-      </p>
-    </div>
-    <div id="about-skills">
-      <span>Skills</span>  
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Jest</li>
-        <li>React & Redux</li>
-        <li>Ruby on Rails</li>
-        <li>PostgreSQL</li>
-      </ul> 
-    </div> 
+    <span id="about-title">ABOUT</span>
+    <span>Get to know more about me</span>
+    <div className="about-content">
+      <div id="about-presentation">
+        <span>
+          Hello
+        </span>
+        <p>
+          My name is Iván Gonzalez, and I am a pasionate Full Stack Web Developer who uses technology 
+          to build engaging websites and applications that empower businesses. Whether you&apos;re launching 
+          a new brand, streamlining operations, or reaching new audiences, 
+          I&apos;m passionate about crafting solutions that make a real difference.
+        </p>
+        <p>
+          Let&apos;s collaborate on your next project! Drop me a line on my contact page, or 
+          let&apos;s have a coffee chat to discuss your ideas.
+        </p>
+        <button id="contact-btn">Contact me</button>
+      </div>
+      <div id="about-skills">
+        <span>Skills</span>  
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React & Redux</li>
+        </ul> 
+        <ul>
+          <li>Bootstrap</li>
+          <li>Jest</li>
+          <li>Ruby on Rails</li>
+        </ul>
+        <ul>
+          <li>PostgreSQL</li>
+          <li>Git</li>
+        </ul> 
+      </div>
+    </div>  
   </div>
 );
 
