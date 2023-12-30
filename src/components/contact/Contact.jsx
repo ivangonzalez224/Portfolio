e6cde06fc948d7cf645d0e62e6f0ae5d14f87fe5
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-30 16:04:34 -0500

    Add styles to the contact form for desktop version

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import '../../assets/styles/contact.css';
 
 const Contact = () => {
   const [fullName, setFullName] = useState('');
@@ -34,37 +35,57 @@ const Contact = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="fullName">Full Name:</label>
-      <input
-        type="text"
-        id="fullName"
-        name="fullName"
-        value={fullName}
-        onChange={(e) => setFullName(e.target.value)}
-        required
-      />
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <label htmlFor="message">Message:</label>
-      <textarea
-        id="message"
-        name="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        required
-      />
-      <button type="submit" disabled={submitting}>
-        {submitting ? 'Sending...' : 'Send Message'}
-      </button>
-    </form>
+    <section className="contact-main">
+      <h2>Contact</h2>
+      <div className="contact-content">
+        <div className="contact-left">
+            <span>I'm always interested in hearing about new projects, so if you'd like to chat,
+                    contact me directly or fill out the form and I will back to you soon.
+            </span>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="fullName">Name</label>
+              <input
+                type="text"
+                className="contact-input"
+                id="fullName"
+                name="fullName"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                required      
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="contact-input"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required        
+            />
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              className="contact-input"
+              name="message"
+              placeholder="Enter your message here..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required        
+            />
+            <button className="contact-submit-btn" type="submit" disabled={submitting}>
+              {submitting ? 'Sending...' : 'Send Message'}
+            </button>
+          </form>
+        </div>
+        <div className="contact-right">
+          <span>EMAIL</span>
+          <span>ivan.gonzalezr224@gmail.com</span>
+          <div className="contact-social"> 
+          </div>
+        </div>
+      </div>
+    </section>    
   );
 };
 
