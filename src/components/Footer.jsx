749796a3bb1cfc42266d478a105591cdcf10cf12
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-01-01 10:05:21 -0500

    Add styles to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
+import '../assets/styles/footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2024 Iván Gonzalez</p>
-      <ul className="social-links">
-        <a className="social-link" href="https://www.linkedin.com/in/iv%C3%A1n-gonzalez/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        <a className="social-link" href="https://github.com/ivangonzalez224" target="_blank" rel="noreferrer"><BsGithub /></a>
+      <ul className="footer-social">
+        <li><a className="footer-link" href="https://www.linkedin.com/in/iv%C3%A1n-gonzalez/" target="_blank" rel="noreferrer"><BsLinkedin /></a></li>
+        <li><a className="footer-link" href="https://github.com/ivangonzalez224" target="_blank" rel="noreferrer"><BsGithub /></a></li>
       </ul>
-      <p id="footer-mail"><a href="mailto:ivan.gonzalezr224@gmail.com"><MdEmail /> Contact</a></p>
+      <p id="footer-mail"><MdEmail />&nbsp;&nbsp;<a href="mailto:ivan.gonzalezr224@gmail.com"> Contact</a></p>
     </footer>
   );
 };
