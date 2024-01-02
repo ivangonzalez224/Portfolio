303597f92ba285b7a0e4638062214c76470d34c5
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-01-02 08:12:54 -0500

    Stick the nav bar on top

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,10 +27,13 @@ const NavBar = () => {
       </ul>
       <ul id="nav-rigth">
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#home">Home</a>
         </li>
         <li>
           <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
