4b2ca038939a5d9c31fbab416c4f459688b6e430
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2026-02-25 18:18:20 -0500

    chore: update footer year and professionalize branding text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ import '../assets/styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Iván Gonzalez</p>
+      <p>&copy; {new Date().getFullYear()} Iván Gonzalez | Full Stack Engineer</p>
       <ul className="footer-social">
         <li><a className="footer-link" href="https://www.linkedin.com/in/iv%C3%A1n-gonzalez/" target="_blank" rel="noreferrer"><BsLinkedin /></a></li>
-        <li><a className="footer-link" href="https://github.com/ivangonzalez224" target="_blank" rel="noreferrer"><BsGithub /></a></li>
+        <li><a className="footer-link" href="https://github.com/ivangonzalez224" target="_blank" rel="noopener noreferrer"><BsGithub /></a></li>
       </ul>
       <p id="footer-mail">
         <a href="mailto:ivan.gonzalezr224@gmail.com">
           <MdEmail />
+          <span className="contact-text">&nbsp;&nbsp;Get in touch</span>
         </a>
-        <span className="contact-text">&nbsp;&nbsp;Contact</span>
       </p>
     </footer>
   );
